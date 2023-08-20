2b712e5330a0f535a8bb44e7bbeb97180bae608d
Guillaume1979 <guibetguillaume@gmail.com>
2023-08-20 19:17:19 +0200

    ADD: set configuration files && configure PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const environment = process.env.ENVIRONMENT === 'prod' ? '.prod' : '.dev';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        'src/config/config.env',
+        `src/config/config${environment}.env`,
+      ],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
+  Logger.log(`Server started on port : ${port}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => Logger.error('Application failed to start', err));
