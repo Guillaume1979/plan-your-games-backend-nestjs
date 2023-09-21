fa86689b6023bd725ef3d28fb50f8652559a4554
Guillaume1979 <guibetguillaume@gmail.com>
2023-09-21 17:27:57 +0200

    FEATURE: add nanoid and write the instruction for the future feature to add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { DiscordAuthGuard } from './guards/discord-auth.guard';
 
 @Controller('auth')
@@ -13,6 +13,20 @@ export class AuthController {
   @UseGuards(DiscordAuthGuard)
   redirect() {
     console.log('redirect');
+    // TODO vérifier si le user est en base
+    // TODO si oui, mettre à jour ses infors Discord
+    // TODO si non, créer le user
+    // TODO générer un code avec nanoid et l'enregistrer dans le cache avec le user
+    // TODO renvoyer le code dans l'URL
     return 'redirect';
+  }
+
+  @Get('discord/getJwtToken/:code')
+  getJwtToken(@Param('code') code: string) {
+    //todo : vérifier que le code est bien dans le cache
+    //todo : vérifier que le code est bien associé à un user
+    //todo : générer un token JWT
+    //todo : renvoyer le token JWT
+    return 'getJwtToken';
   }
 }
