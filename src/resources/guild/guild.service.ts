faa8d61f72ac1c72718f7207f529f4f5dc6eace6
Guillaume1979 <guibetguillaume@gmail.com>
2024-03-25 14:34:22 +0100

    Chore: implement the findAll method for guilds

--- src/resources/guild/guild.service.ts
+++ src/resources/guild/guild.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGuildDto } from './dto/create-guild.dto';
 import { UpdateGuildDto } from './dto/update-guild.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Guild } from './entities/guild.entity';
 
 @Injectable()
 export class GuildService {
+  constructor(@InjectRepository(Guild) private readonly guildRepository: Repository<Guild>) {}
   create(createGuildDto: CreateGuildDto) {
     return 'This action adds a new guild';
   }
 
-  findAll() {
-    return `This action returns all guild`;
+  async findAll() {
+    return await this.guildRepository.find();
   }
 
   findOne(id: number) {
